10ff348fbb32ca724dc8f2dc32aab081d1caba2c
Long Ng <18120134@student.hcmus.edu.vn>
2021-01-08 11:46:49 +0700

    Update convert.py
    fix resize convert.py

--- jit/convert.py
+++ jit/convert.py
@@ -13,8 +13,7 @@ if __name__ == "__main__":
 
     transform_val = transforms.Compose(
         [
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
+            transforms.Resize((224,224)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
                 0.229, 0.224, 0.225]),
